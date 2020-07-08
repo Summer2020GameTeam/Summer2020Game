84a7c057a73f128c0ff038deaba80fcb51b153e3
Wesley <wesley.verhagen@student.fontys.nl>
2020-07-08 14:07:51 +0200

    Added water

---
+++ Summer2020Project/Assets/Scripts/CameraChaseComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChaseComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Target;
+    [SerializeField]
+    private bool DoesLerp;
+    [SerializeField]
+    private float LerpValue = .2f;
+    [SerializeField]
+    private Vector3 PositionOffset;
+
+    private void LateUpdate()
+    {
+        MoveCamToPosition();
+    }
+
+    private void MoveCamToPosition()
+    {
+        if (DoesLerp)
+        {;
+            LerpCamPosition();
+        }
+        else
+        {
+            transform.position = Target.position + PositionOffset;
+        }
+    }
+
+    void LerpCamPosition()
+    {
+            Vector3 newPosition = Vector3.Lerp(transform.position, Target.position, LerpValue);
+            transform.position = newPosition + PositionOffset;
+    }
+}
---
+++ Summer2020Project/Assets/Scripts/TextureCamTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCamTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Camera textureCam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            textureCam.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            textureCam.enabled = false;
+        }
+    }
+}
