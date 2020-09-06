32a0286984bba8e6cfc6bf67db89c88957b95a97
Wesley <wesley.verhagen@student.fontys.nl>
2020-09-06 10:49:58 +0200

    Implemented scene loading
    Basic scene loading with a request based system now implemented.
    An issue is still present when leaving a room with no connecting room

---
+++ Summer2020Project/Assets/Scripts/RoomLoadingManagerComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomLoadingManagerComponent : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> ConnectedScenes;
+    private bool hasLoaded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasLoaded && collision.gameObject.tag == "Player")
+        {
+            foreach (string scene in ConnectedScenes)
+            {
+                RoomLoadingMaster.Master.RequestSceneLoad(scene);
+            }
+            hasLoaded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (hasLoaded && collision.gameObject.tag == "Player")
+        {
+            foreach (string scene in ConnectedScenes)
+            {
+                RoomLoadingMaster.Master.RequestSceneUnload(scene);
+            }
+            hasLoaded = false;
+        }
+
+    }
+}
---
+++ Summer2020Project/Assets/Scripts/RoomLoadingMaster.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomLoadingMaster : MonoBehaviour
+{
+    public static RoomLoadingMaster Master;
+
+    private EventList<string> SceneLoadRequests = new EventList<string>();
+    private EventList<string> SceneUnloadRequests = new EventList<string>();
+    private List<string> LoadedScenes = new List<string>();
+
+    public class EventList<T> : List<T>
+    {
+        public event ListChangedEventDelegate ListChanged;
+        public delegate void ListChangedEventDelegate();
+
+        public new void Add(T item)
+        {
+            base.Add(item);
+            if (ListChanged != null
+                && ListChanged.GetInvocationList().Any())
+            {
+                ListChanged();
+            }
+        }
+    }
+
+    public void RequestSceneLoad(string scene)
+    {
+        SceneLoadRequests.Add(scene);
+        SceneUnloadRequests.Remove(scene);
+    }
+
+    public void RequestSceneUnload(string scene)
+    {
+        SceneLoadRequests.Remove(scene);
+        SceneUnloadRequests.Add(scene);
+    }
+
+    private void Awake()
+    {
+        if(Master == null)
+        {
+            Master = this;
+        }
+
+        CheckForLoadedScenes();
+
+        SceneLoadRequests.ListChanged += HandleLoadRequests;
+        SceneUnloadRequests.ListChanged += HandleUnloadRequests;
+    }
+
+    private void CheckForLoadedScenes()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            LoadedScenes.Add(SceneManager.GetSceneAt(i).name);
+        };
+    }
+
+    private void HandleLoadRequests()
+    {
+        List<string> ScenesToLoad = new List<string>();
+
+        foreach (string request in SceneLoadRequests)
+        {
+            if (!LoadedScenes.Contains(request))
+            {
+                Debug.Log("Load " + request);
+                ScenesToLoad.Add(request);
+            }
+        }
+
+        foreach (string request in ScenesToLoad)
+        {
+            SceneManager.LoadSceneAsync(request, LoadSceneMode.Additive);
+            LoadedScenes.Add(request);
+        }
+    }
+
+    private void HandleUnloadRequests()
+    {
+        List<string> ScenesToUnload = new List<string>();
+
+        foreach (string request in SceneUnloadRequests)
+        {
+            if (!SceneLoadRequests.Contains(request))
+            {
+                Debug.Log("Unload " + request);
+                ScenesToUnload.Add(request);
+            }
+        }
+
+        foreach (string request in ScenesToUnload)
+        {
+            SceneManager.UnloadSceneAsync(request);
+            LoadedScenes.Remove(request);
+        }
+    }
+
+}
